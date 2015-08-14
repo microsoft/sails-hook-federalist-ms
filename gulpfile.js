04a5016c8a7adf2f8df8c142308326afbdf597f0
Andrew Weiss <andrew.weiss@microsoft.com>
2015-08-14 16:43:13 -0400

    updated docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var exec = require('child_process').exec;
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
+var clean = require('gulp-clean');
 
 gulp.task('test', function() {
 	return gulp.src(['test/*.spec.js'], { read: false})
@@ -18,7 +19,12 @@ gulp.task('test', function() {
 		.on('error', gutil.log);
 });
 
-gulp.task('docs', function(cb) {
+gulp.task('clean-docs', function() {
+	return gulp.src('jsdocs', { read: false })
+		.pipe(clean());
+});
+
+gulp.task('docs', ['clean-docs'], function(cb) {
 	exec('jsdoc --configure ./.jsdoc.json --verbose', function (err, stdout, stderr) {
 		console.log(stdout);
 		console.log(stderr);

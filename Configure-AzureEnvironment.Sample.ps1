33e51053594dcbd918c467daab8c849a635dc872
Andrew Weiss <andrew.weiss@microsoft.com>
2015-08-18 18:45:26 -0400

    update env var sample scripts

--- Configure-AzureEnvironment.Sample.ps1
+++ Configure-AzureEnvironment.Sample.ps1
@@ -1,3 +1,6 @@
+# buildEngine
+$env:FEDERALIST_BUILD_ENGINE = ''
+
 # Subscrption ID
 $env:FEDERALIST_AZURE_SUBSCRIPTION_ID = ''
 
@@ -11,23 +14,5 @@ $env:FEDERALIST_AZURE_PASSWORD = ''
 # Azure AD Application Client ID
 $env:FEDERALIST_AZURE_CLIENT_ID = ''
 
-# Resource Group Name for site build
-$env:FEDERALIST_AZURE_RG_NAME = ''
-
 # Region for new Resource Group and Web App
 $env:FEDERALIST_AZURE_REGION = ''
-
-# Path to Resource Group template file
-$env:FEDERALIST_AZURE_RG_TEMPLATE_PATH = ''
-
-# Resource Group deployment name
-$env:FEDERALIST_AZURE_RG_DEPLOYMENT_NAME = ''
-
-# New/existing Azure Web App name (if existing, making sure it resides within the Resource Group defined)
-$env:FEDERALIST_AZURE_WEBAPP_NAME = ''
-
-# New App Hosting Plan name
-$env:FEDERALIST_AZURE_APPHOSTINGPLAN_NAME = ''
-
-# Local site directory to publish to Azure Web App (for testing purposes; normally defined by buildEngine tokens)
-$env:FEDERALIST_AZURE_LOCAL_SITE_DIRECTORY = ''
